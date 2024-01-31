1d9032de59c9d99b5f466644c7610a2bac8ae9db
Park Seo Yeon <syss220211@gmail.com>
2024-01-31 15:16:22 +0900

    feat: Think 폰트 추가
    Pretendard 네이밍 CustomFont로 수정

--- Projects/App/Sources/Design/Font.swift
+++ Projects/App/Sources/Design/Font.swift
@@ -8,22 +8,24 @@
 
 import SwiftUI
 
-struct Pretendard {
+struct CustomFont {
     static let medium = "Pretendard-Medium"
     static let bold = "Pretendard-Bold"
     static let regular = "Pretendard-Regular.otf"
     static let semiBold = "Pretendard-SemiBold.otf"
+    static let thinkRegular = "Think-Regular.otf"
 }
 
 extension Font {
     // Bold
-    static let pretendardBold28: Font = .custom(Pretendard.bold, size: 28)
-    static let pretendardBold20: Font = .custom(Pretendard.bold, size: 20)
-    static let pretendardBold14: Font = .custom(Pretendard.bold, size: 14)
+    static let pretendardBold28: Font = .custom(CustomFont.bold, size: 28)
+    static let pretendardBold20: Font = .custom(CustomFont.bold, size: 20)
+    static let pretendardBold14: Font = .custom(CustomFont.bold, size: 14)
     
     // Regular
-    static let pretendardRegular14: Font = .custom(Pretendard.regular, size: 14)
-    static let pretendardRegular10: Font = .custom(Pretendard.regular, size: 10)
+    static let pretendardRegular14: Font = .custom(CustomFont.regular, size: 14)
+    static let pretendardRegular10: Font = .custom(CustomFont.regular, size: 10)
+    static let thinkRegular80: Font = .custom(CustomFont.thinkRegular, size: 80)
     
     // SemiBold
     
